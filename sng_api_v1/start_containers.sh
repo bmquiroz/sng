962fc26844d2200faed8466ff00bb947275c8fba
bmquiroz <bmquiroz@outlook.com>
2020-06-01 16:51:17 -0400

    updates

--- sng_api_v1/start_containers.sh
+++ sng_api_v1/start_containers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Get AD secrets
-CREDS=`aws --region us-east-1 secretsmanager get-secret-value --secret-id domain.joiner | jq --raw-output '.SecretString' | jq -r '."domain.joiner"'`
+# DB_CREDS=`aws --region us-east-1 secretsmanager get-secret-value --secret-id domain.joiner | jq --raw-output '.SecretString' | jq -r '."domain.joiner"'`
 
-docker-compose -f docker-compose.prod.yml up -d --build $CREDS
+docker-compose -f docker-compose.prod.yml up -d --build 
